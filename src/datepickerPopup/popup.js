614f5235a45552ab437cfb59262ddc29b856ee02
Patricia Carbajal <patricia@Patricias-MacBook-Pro-2.local>
2017-05-19 11:13:17 -0400

    chore(release): 2.5.1

--- src/datepickerPopup/popup.js
+++ src/datepickerPopup/popup.js
@@ -264,6 +264,7 @@ function($scope, $element, $attrs, $compile, $log, $parse, $window, $document, $
   }
 
   $scope.$watch('isOpen', function(value) {
+    var dpElement = angular.element($popup[0].querySelector('.uib-datepicker-popup'));
     if (value) {
       if (!$scope.disabled) {
         $timeout(function() {
@@ -406,6 +407,7 @@ function($scope, $element, $attrs, $compile, $log, $parse, $window, $document, $
       var placement = $attrs.popupPlacement ? $attrs.popupPlacement : datepickerPopupConfig.placement;
       var position = $position.positionElements($element, dpElement, placement, appendToBody);
       dpElement.css({top: position.top + 'px', left: position.left + 'px'});
+      dpElement.addClass('show');
       if (dpElement.hasClass('uib-position-measure')) {
         dpElement.removeClass('uib-position-measure');
       }
